3b37774bee5a645df75bebcc94dfb5a63e843f5a
Opilane TTHK <opilane@tthk.ee>
2019-09-13 17:14:47 +0300

    lession2 string

--- lession2/Program.cs
+++ lession2/Program.cs
@@ -9,6 +9,30 @@ namespace lession2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+            string firstName;
+            string lastName;
+            string favouriteColor;
+            Console.WriteLine("Enter your first name");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            lastName = Console.ReadLine();
+            //Console.WriteLine("Nice to meet you, " + firstName + " " + lastName + "!");
+            //i´m just a comment
+            Console.WriteLine("enter your favourite color");
+            favouriteColor = Console.ReadLine();
+            Console.WriteLine("Nice to meet you {0} {1}, your favourite color is {2}." , firstName, lastName, favouriteColor );
+            Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite color?");
+            //Console.ReadLine();
+            
+            
+            
+            
+            
+        
         }
     }
 }
